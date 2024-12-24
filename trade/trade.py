330fac2a75cb9c59a559b39dc8d302c35233368a
havison <dieu2010@mail.ru>
2024-12-24 17:23:39 +1000

    23.12.2024

--- trade/trade.py
+++ trade/trade.py
@@ -46,7 +46,7 @@ async def get_symbol_price(symbol):
         ticker = client.get_tickers(category="linear", symbol=symbol)
         x = ticker['result']['list'][0]['lastPrice']
         v = ticker['result']['list'][0]['volume24h']
-        if float(v) * float(x) < 5000000:
+        if abs(float(v) * float(x)) < 5000000:
             logger2.info('Маленький объем, завершаю скрипт')
             return False
         return float(x)
