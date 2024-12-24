ac1dd908301086241f18ebb30b3c8723a2e9e414
havison <dieu2010@mail.ru>
2024-12-24 16:53:36 +1000

    23.12.2024

--- trade/trade.py
+++ trade/trade.py
@@ -17,8 +17,8 @@ client = HTTP(testnet=False, api_key=config.by_bit.api_key, api_secret=config.by
 
 trade_amount = 25 # Размер сделки в USDT
 stop_loss_pct = 2  # Стоп-лосс в процентах
-take_profit_pct = 1.5  # Уровень активации трейлинг-стопа
-trailing_stop_pct = 2.5
+take_profit_pct = 2.5  # Уровень активации трейлинг-стопа
+trailing_stop_pct = 1.5
 take_profit = 5
 # Расстояние трейлинг-стопа
 
